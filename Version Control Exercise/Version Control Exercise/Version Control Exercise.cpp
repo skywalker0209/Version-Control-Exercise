52c2662096ad58fa96911105622b8ca2f7760521
skywalker0209 <s221073@students.aie.edu.au>
2023-03-26 18:54:12 +1100

    Sort Descending

--- Version Control Exercise/Version Control Exercise/Version Control Exercise.cpp
+++ Version Control Exercise/Version Control Exercise/Version Control Exercise.cpp
@@ -7,13 +7,14 @@ int main()
 	int numbers[array_numbers] = { 1,2,3,4,5,6,7,8,9,10 };
 
 	/*
-	TODO: Print each value of the array in order
-	Expected:
-		1,2,3,4,5,6,7,8,9,10
+	TODO: This should be in descending order
+		10,9,8,7,6,5,4,3,2,1
 	*/
-	for (int i = 0; i < array_numbers; i++)
+	
+	for (int i = array_numbers-1; i >= 0; i--)
 	{
 		cout << numbers[i] << ", ";
 	}
 }
+
 
